53471b8cbfabc29ba0c372b9c64429a599dd3a60
fdetsch <florian.detsch@staff.uni-marburg.de>
2017-08-15 17:14:40 +0200

    Update: minor edits related to data reprocessing.

--- R/preprocess.R
+++ R/preprocess.R
@@ -9,58 +9,53 @@ lib <- c("MODIS", "doParallel", "rworldmap")
 jnk <- sapply(lib, function(x) library(x, character.only = TRUE))
 
 ## working directory
-setwd("/media/dogbert/modis_data")
+setwd("/media/fdetsch/modis_data")
 
 ## parallelization
 cl <- makeCluster(detectCores() - 1)
 registerDoParallel(cl)
 
 ## modis options
-MODISoptions(localArcPath = "MODIS_ARC/", outDirPath = "MODIS_ARC/PROCESSED/")
+MODISoptions(localArcPath = "MODIS_ARC/", outDirPath = "MODIS_ARC/PROCESSED/", 
+             outProj = "+init=epsg:4326")
 
 
 ### data download -----
 
-# ## reference extent
-# ext <- subset(countriesCoarse, ADMIN.1 == "South Africa")
-# 
-# ## download data in parallel
-# foreach(product = c("MOD14A1", "MYD14A1"), .packages = "MODIS") %dopar%
-#   runGdal(product, extent = ext, job = paste0(product, ".006"),
-#           collection = getCollection(product, forceCheck = TRUE))
+## reference extent
+ext <- subset(countriesCoarse, ADMIN.1 == "South Africa")
+
+## download data in parallel
+clc = getCollection("M*D14A1", forceCheck = TRUE)
+tfs = runGdal("M*D14A1", extent = ext, job = "MCD14A1.006",
+              collection = clc[[1]])
 
 
 ### data processing -----
 
 ## loop over products
-lst_prd <- lapply(c("MOD14A1.006", "MYD14A1.006"), function(product) {
+lst_prd <- lapply(names(tfs), function(product) {
   
-  dir_prd <- paste0("/media/dogbert/dev/data/", product)
-  if (!dir.exists(dir_prd)) dir.create(dir_prd)
-  
+  dir_prd <- paste0("/media/fdetsch/XChange/safire/", product)
+  jnk = Orcs::ifMissing(dir_prd, file.path, dir.create, arg1 = "path")
+
   ## crop images
-  rst <- foreach(i = c("FireMask", "QA", "MaxFRP", "sample")) %do% {                                      
+  rst <- foreach(i = 1:4) %do% {                                      
                        
     # list and import available files
-    fls <- list.files(paste0(getOption("MODIS_outDirPath"), "/", product),
-                      pattern = paste0(i, ".tif$"), full.names = TRUE)
-    
-    if (i %in% c("FireMask", "QA", "sample")) {
+    fls = sapply(tfs[[product]], "[[", i)
+
+    if (length(grep("FireMask.tif$|QA.tif$|sample.tif$", fls[1])) == 1) {
       rst <- raster::stack(fls)
     } else {
                        
       # apply scale factor
       dir_scl <- paste0(dir_prd, "/scl")
-      if (!dir.exists(dir_scl)) dir.create(dir_scl)
+      jnk = Orcs::ifMissing(dir_scl, file.path, dir.create, arg1 = "path")
       
       lst <- foreach(j = 1:length(fls), .packages = lib, 
                      .export = ls(envir = globalenv())) %dopar% {
         rst <- stack(fls[j])
-        lst <- unstack(rst)
-        rst <- stack(lapply(lst, function(k) {
-          dataType(k) <- "INT4U"
-          return(k)
-        }))
         
         fls_scl <- paste0(dir_scl, "/", names(rst), ".tif")
         
@@ -101,7 +96,7 @@ lst_prd <- lapply(c("MOD14A1.006", "MYD14A1.006"), function(product) {
             } else {
               reclassify(rst[[1]][[j]], rcl, include.lowest = TRUE, 
                          right = FALSE, filename = fls_rcl[j], 
-                         format = "GTiff", overwrite = TRUE)
+                         overwrite = TRUE, datatype = "INT1U")
             }
           }
   
@@ -111,22 +106,22 @@ lst_prd <- lapply(c("MOD14A1.006", "MYD14A1.006"), function(product) {
   ## create annual frequency images
 
   # indices
-  dts <- extractDate(names(rst[[1]]))$inputLayerDates
+  nms = basename(sapply(tfs[[product]], "[[", 1))
+  dts <- extractDate(nms)$inputLayerDates
   yrs <- substr(dts, 1, 4)
-  ids <- as.numeric(as.factor(yrs))
-  
+
   # target folder and files
   dir_yrs <- paste0(dir_prd, "/yrs")
   if (!dir.exists(dir_yrs)) dir.create(dir_yrs)
   
   fls_yrs <- sapply(unique(yrs), function(j) {
-    gsub(dts[1], j, names(rst[[1]])[1])
+    gsub(dts[1], j, nms[1])
   })
   fls_yrs <- paste0(dir_yrs, "/", fls_yrs, ".tif")
   
-  for (j in seq(unique(ids))) {
+  for (j in seq(unique(yrs))) {
     if (!file.exists(fls_yrs[j])) {
-      rst_yr <- rst_rcl[[which(ids == unique(ids)[j])]]
+      rst_yr <- rst_rcl[[which(yrs == unique(yrs)[j])]]
       calc(rst_yr, fun = function(x) {
         sum(x, na.rm = TRUE) / length(x)
       }, filename = fls_yrs[j], format = "GTiff", overwrite = TRUE)
